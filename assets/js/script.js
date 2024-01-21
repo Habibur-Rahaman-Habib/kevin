00fccff25bc209a07c67258a87879b118f6a8708
Habibur Rahaman <habib5501659@gmail.com>
2024-01-21 14:41:17 +0600

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -392,6 +392,12 @@
         }
     });
 
+    // 14. data background
+    $('[data-background]').each(function () {
+        $(this).css('background-image', 'url(' + $(this).attr('data-background') + ')');
+    });
+
+
 
 })(jQuery)
 
